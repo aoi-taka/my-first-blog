1a11f0dbf546c190dc38d15ad15a00ef4b913053
testuser <neo.aoi.takahashi@gmail.com>
2021-07-07 11:27:46 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	#post_listという名前のビューをルートURLに割り当て＝
 	path('',views.post_list,name = 'post_list'),
+	path('post/<int:pk>/',views.post_detail,name = 'post_detail'),
 ]
